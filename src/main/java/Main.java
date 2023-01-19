b0d86093e63029da654cad60f3b9c1ac660e52d5
Павел Мешалкин <google5541@mail.ru>
2023-01-20 00:18:49 +0400

    lust commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ import java.net.URLConnection;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         URL url = new URL("https://drive.google.com/u/0/uc?id=1aZmsPZNHi-yuyOdsnG8w1dA7G4sV6zAD&export=download");
         URLConnection con = url.openConnection();
         InputStream is = con.getInputStream();
@@ -16,50 +16,52 @@ public class Main {
         List<City> citiesList = new ArrayList<>();
         while (reader.read() != -1) {
             String str = reader.readLine();
-                String[] stringsArray = str.split(";");
-                if (stringsArray.length == 6) {
-                    citiesList.add(new City(stringsArray[1], stringsArray[2], stringsArray[3], Integer.parseInt(stringsArray[4]), stringsArray[5]));
-                }
-                if (stringsArray.length == 5) {
-                    citiesList.add(new City(stringsArray[1], stringsArray[2], stringsArray[3], Integer.parseInt(stringsArray[4]), null));
-                }
+            String[] stringsArray = str.split(";");
+            if (stringsArray.length == 6) {
+                citiesList.add(new City(stringsArray[1], stringsArray[2], stringsArray[3], Integer.parseInt(stringsArray[4]), stringsArray[5]));
+            }
+            if (stringsArray.length == 5) {
+                citiesList.add(new City(stringsArray[1], stringsArray[2], stringsArray[3], Integer.parseInt(stringsArray[4]), null));
+            }
         }
 
         citiesList.sort(Comparator.comparing(City::getRegion)
                 .thenComparing(City::getName));
 
-//        getMaxPopulationOfCity(citiesList);
+        System.out.println(citiesList.size());
+        for (City city : citiesList) {
+            System.out.println(city.toString());
+        }
+
+        getMaxPopulationOfCity(citiesList);
 
         countCityOfRegion(citiesList);
 
-//        System.out.println(citiesList.size());
-//        for (City city : citiesList) {
-//            System.out.println(city.toString());
-//        }
+
         reader.close();
         is.close();
 
     }
 
-    public static void getMaxPopulationOfCity(List<City> list){
+    public static void getMaxPopulationOfCity(List<City> list) {
         int i = 0;
         int position = 0;
         int population = 0;
-        for (City c : list){
-            if (c.getPopulation()>population){
+        for (City c : list) {
+            if (c.getPopulation() > population) {
                 position = i;
                 population = c.getPopulation();
             }
             i++;
         }
-        System.out.println("["+position+"] = "+population);
+        System.out.println("[" + position + "] = " + population);
     }
 
-    public static void countCityOfRegion(List<City> list){
+    public static void countCityOfRegion(List<City> list) {
         int countCity = 0;
         String region = list.get(0).getRegion();
-        for (City c : list){
-            if (c.getRegion().equals(region)){
+        for (City c : list) {
+            if (c.getRegion().equals(region)) {
                 countCity++;
             } else {
                 System.out.println(region + " - " + countCity);
