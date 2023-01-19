a8b1d04f469ad57ccee284f7ed2c07ff583825f6
Павел Мешалкин <google5541@mail.ru>
2023-01-19 17:12:04 +0400

    create list sorting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -14,8 +13,8 @@ public class Main {
         URLConnection con = url.openConnection();
         InputStream is = con.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        ArrayList<City> citiesList = new ArrayList<>();
-        while (reader.read() != -1){
+        List<City> citiesList = new ArrayList<>();
+        while (reader.read() != -1) {
             String str = reader.readLine();
                 String[] stringsArray = str.split(";");
                 if (stringsArray.length == 6) {
@@ -25,6 +24,18 @@ public class Main {
                     citiesList.add(new City(stringsArray[1], stringsArray[2], stringsArray[3], Integer.parseInt(stringsArray[4]), null));
                 }
         }
+
+        Collections.sort(citiesList, new Comparator<City>() {
+            @Override
+            public int compare(City o1, City o2) {
+                boolean bool = o1.getRegion().equals(o2.getRegion());
+                if (bool) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+                return 1;
+            }
+        });
+
         System.out.println(citiesList.size());
         for (City city : citiesList) {
             System.out.println(city.toString());
